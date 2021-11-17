da4eb701c12c52b17a29f5076da441d12c794f7e
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-11-17 15:16:03 +0200

    tehtävä 1

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,32 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
 
-if __name__ == "__main__":
-    main()
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        if player_dict["nationality"] == "FIN":
+            player = Player(
+                player_dict['name'],
+                player_dict["nationality"],
+                player_dict["assists"],
+                player_dict["goals"],
+                player_dict["penalties"],
+                player_dict["team"],
+                player_dict["games"]
+            )
+
+            players.append(player)
+
+    print("Finnish players:")
+
+    for player in players:
+        print(player)
+
+main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
+        self.points = self.assists+self.goals 
     
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.team}: {self.assists} assists, {self.goals} goals, {self.penalties} penalties in {self.games} games"
