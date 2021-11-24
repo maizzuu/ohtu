d7ae53a248824c2171420db765f1ecd8a8333551
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-11-24 11:04:59 +0200

    lisaa tuote test fixed

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -18,7 +18,7 @@ class Ostoskori:
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen, tarkistaa onko sitä jo valmiiksi
-        pass
+        self.kori.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
