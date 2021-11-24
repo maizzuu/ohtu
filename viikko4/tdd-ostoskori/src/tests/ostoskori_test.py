3e704eeb200fe14cd2f1c583cbd516bedda79912
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-11-24 11:01:23 +0200

    lisaa tuote test created

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,6 +5,11 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.tuote1 = Tuote("peruna", 3)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
+
+    def test_lisaa_tuote_jota_ei_ole_jo_korissa(self):
+        self.kori.lisaa_tuote(self.tuote1)
+        self.assertEqual(len(self.kori.kori), 1)
