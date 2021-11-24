2d5d23630924e6986863b4c560dfd2165dbf11a6
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-11-24 11:49:19 +0200

    testikoodia

--- viikko4/tdd-ostoskori/src/index.py
+++ viikko4/tdd-ostoskori/src/index.py
@@ -1,1 +1,19 @@
-# testikoodi tänne jos tarvetta
+from ostoskori import Ostoskori
+from tuote import Tuote
+from ostos import Ostos
+
+def main():
+    ostoskori = Ostoskori()
+    peruna = Tuote("peruna", 3)
+    ostoskori.lisaa_tuote(peruna)
+    for ostos in ostoskori.kori:
+        print(ostos.tuotteen_nimi())
+        print(ostos.lukumaara())
+    ostoskori.lisaa_tuote(peruna)
+    for ostos in ostoskori.kori:
+        print(ostos.tuotteen_nimi())
+        print(ostos.lukumaara())
+
+
+if __name__ == "__main__":
+    main()
