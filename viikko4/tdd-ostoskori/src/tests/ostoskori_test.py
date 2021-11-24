020ce979446a0e6d875cf8ee41f8e0dc2afe6fe6
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-11-24 11:10:56 +0200

    Yhden tuotteen lisäämisen jälkeen ostoskorissa on 1 tavara

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,12 +5,15 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
-        self.tuote1 = Tuote("peruna", 3)
+        self.peruna = Tuote("peruna", 3)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
-    def test_lisaa_tuote_jota_ei_ole_jo_korissa(self):
-        self.kori.lisaa_tuote(self.tuote1)
-        self.assertEqual(len(self.kori.kori), 1)
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
+        self.kori.lisaa_tuote(self.peruna)
+        
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    
