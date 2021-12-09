3175e8653f1e9b11b20478493094f6a1955466b3
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-12-09 13:21:40 +0200

    tehtävä 2

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -25,3 +25,33 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class All:
+    def __init__(self, value, attr):
+        self._attr = attr
+        self._value = value
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value == self._value
+
+class Not:
+    def __init__(self, olio):
+        self._olio = olio
+
+    def matches(self, player):
+        if self._olio.matches(player):
+            return False
+        return True
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
+    
